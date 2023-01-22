5f5ea7a2ef7e4109a38ec207e6136e2d60ba8e85
Simon Bilodeau <79489836+Bilowdz@users.noreply.github.com>
2023-01-22 01:20:36 -0500

    rotates on correct point
    except it only does it once, needs to be able to rotate over and over

---
+++ My project/Assets/activateObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class activateObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+ 
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+ 
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+ 
+            RaycastHit hit;
+ 
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "ship")
+                {
+ 
+                    var objectScript = hit.collider.GetComponent<DragAndRotate>();
+                    objectScript.isActive = !objectScript.isActive;
+                }
+            }
+        }
+    }
+}
