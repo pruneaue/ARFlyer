db8ec598b0f410c27a9764a3444ddd247c4f645c
Simon Bilodeau <79489836+Bilowdz@users.noreply.github.com>
2023-01-22 06:00:47 -0500

    back to unlimited rotates

--- My project/Assets/activateObjects.cs
+++ My project/Assets/activateObjects.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
  
-public class activateObject : MonoBehaviour
+public class activateObjects : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
