7739980b48085ba2dda040d37913a5a6f83384f5
RyanHaniff <haniffryan.hr@gmail.com>
2023-01-22 04:53:42 -0500

    Added play pause buttons

---
+++ My project/Assets/PlayVideo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+
+    private VideoPlayer video;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        video.Play();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
